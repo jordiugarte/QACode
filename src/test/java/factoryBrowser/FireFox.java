541412ed592809a18194c6c354520132d7f2b20e
jordiugarte <jordi933@gmail.com>
2021-03-10 07:04:54 -0400

    Cajero

--- src/test/java/factoryBrowser/FireFox.java
+++ src/test/java/factoryBrowser/FireFox.java
@@ -1,10 +1,16 @@
 package factoryBrowser;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class FireFox implements IBrowser {
     @Override
     public WebDriver create() {
-        return null;
+        System.setProperty("webdriver.firefoxdriver.driver", "src/test/resources/firefoxDriver/geckodriver.exe");
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        return driver;
     }
 }
